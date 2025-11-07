c621ea34f09e29f27f6a7f7d7eca10aab899a78e
PharesJr <pharesirungu@gmail.com>
2025-11-07 11:18:37 +0300

    AI model Llama-4

--- app/api/sms/route.js
+++ app/api/sms/route.js
@@ -6,7 +6,7 @@ import { AzureKeyCredential } from "@azure/core-auth";
 // Initialize AI Model Client
 const token = process.env["GITHUB_TOKEN"];
 const endpoint = "https://models.github.ai/inference";
-const model = "openai/gpt-5";
+const model =  "meta/Llama-4-Scout-17B-16E-Instruct";;
 const client = ModelClient(endpoint, new AzureKeyCredential(token));
 
 // In-memory store for conversation history
